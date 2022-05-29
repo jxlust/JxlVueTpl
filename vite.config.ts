b753109f625d9b0eea1e42f71ed8c03edb4bc6e4
xunliang jiang <xunliangjiang@xunliangdeMacBook-Pro.local>
2022-05-29 21:17:15 +0800

    chore: filemanager hooks

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,8 @@ import AutoImport from 'unplugin-auto-import/vite';
 import Components from 'unplugin-vue-components/vite';
 import WindiCSS from 'vite-plugin-windicss';
 import compressPlugin from 'vite-plugin-compression';
+const ViteFilemanager = require('filemanager-plugin').ViteFilemanager;
+
 const CWD = process.cwd();
 
 // https://cn.vitejs.dev/config/
@@ -59,6 +61,47 @@ export default ({ mode }: ConfigEnv): UserConfig => {
         ext: '.gz',
         deleteOriginFile: false, // 是否删除原始文件
       }),
+      ViteFilemanager({
+        // events: {
+        //   start: {
+        //     del: {
+        //       items: ['./dist'],
+        //     },
+        //   },
+        //   end: {
+        //     move: {
+        //       items: [{ source: './www', destination: './dist' }],
+        //     },
+        //     zip: {
+        //       items: [{ source: './dist', destination: './dist.zip', type: 'zip' }],
+        //     },
+        //     del: {
+        //       // items: ['./dist','./www'],
+        //     },
+        //   },
+        // },
+        customHooks: [
+          {
+            hookName: 'generateBundle', //generateBundle
+            commands: {
+              move: {
+                items: [{ source: './www', destination: './dist/' }],
+              },
+              zip: {
+                items: [{ source: './dist', destination: './dist.zip', type: 'zip' }],
+              },
+              del: {
+                items: ['./dist', './www'],
+              },
+            },
+          },
+        ],
+        options: {
+          // parallel: 3,
+          log: 'all',
+          cache: false,
+        },
+      }),
     ],
     resolve: {
       alias: [
@@ -72,7 +115,7 @@ export default ({ mode }: ConfigEnv): UserConfig => {
       cssCodeSplit: true, // 如果设置为false，整个项目中的所有 CSS 将被提取到一个 CSS 文件中
       sourcemap: mode !== 'production', // 构建后是否生成 source map 文件。如果为 true，将会创建一个独立的 source map 文件
       target: 'modules', // 设置最终构建的浏览器兼容目标。默认值是一个 Vite 特有的值——'modules'  还可设置为 'es2015' 'es2016'等
-      outDir: 'dist-tpl', //指定输出路径
+      outDir: 'www', //指定输出路径
       assetsDir: 'assets', // 指定生成静态资源的存放路径
       chunkSizeWarningLimit: 550, // 单位kb  打包后文件大小警告的限制 (文件大于此此值会出现警告)
       assetsInlineLimit: 4096, // 单位字节（1024等于1kb） 小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项。
