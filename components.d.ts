951f21eb577f214df7c52ebd01869e10b75c24f5
xunliang jiang <xunliangjiang@xunliangdeMacBook-Pro.local>
2022-05-29 11:01:21 +0800

    style: add css

--- components.d.ts
+++ components.d.ts
@@ -2,6 +2,7 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 import 'vue';
+
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton'];
