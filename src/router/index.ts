4b34c2dbecd999696084e131a261f264e0de445f
jxlust <jxljxufe@163.com>
2022-06-07 17:43:14 +0800

    feat: apitest

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,16 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: jxlust jxljxufe@163.com
+ * @Date: 2022-05-28 21:38:12
+ * @LastEditors: jxlust
+ * @LastEditTime: 2022-06-07 16:53:15
+ */
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 const HelloWorld = () => import('@/components/HelloWorld.vue');
 const TestVue = () => import('@/views/TsxVue/index');
 const Test = () => import('@/views/Test/index.vue');
+const ApiTest = () => import('@/package/ApiTest/index.vue');
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
@@ -35,6 +44,15 @@ const routes: Array<RouteRecordRaw> = [
       keepAlive: true,
     },
     component: Test,
+  },
+  {
+    path: '/vueApi',
+    name: 'vueApi',
+    meta: {
+      title: 'vueApi study',
+      keepAlive: true,
+    },
+    component: ApiTest,
   },
 ];
 
