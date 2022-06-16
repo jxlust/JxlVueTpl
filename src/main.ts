ef8bab7a86e8430433983e8d05dee15048d6541d
jxlust <jxljxufe@163.com>
2022-06-16 11:18:24 +0800

    feat: mock & defineoptions

--- src/main.ts
+++ src/main.ts
@@ -15,4 +15,11 @@ setupStore(app);
 // app.use(BaseComponens)
 // app.use(BaseDirective)
 
+// import命令会被 JavaScript 引擎静态分析，不能放在代码块里（if代码中）
+// import()函数支持动态加载模块和按条件导入，可以放在代码块里（if代码中）
+// 至于true为什么要加引号，大家体验一下不加的情况就能明白了，.env.development文件中的value都自动加了引号。
+// console.log(process.env.NODE_ENV,typeof process.env.NODE_ENV)
+console.log(111, process.env.NODE_ENV);
+process.env.NODE_ENV === 'development' && import('./mock');
+
 app.use(router).mount('#app');
