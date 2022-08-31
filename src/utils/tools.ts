789f9ffee8b9477f99d00124b5703116d0689523
jxlust <jxljxufe@163.com>
2022-08-31 21:55:55 +0800

    feat: tribute pk

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,1 +1,17 @@
 export const extend = Object.assign;
+
+export function escapeSpecialCharacter(str: string) {
+  const pattern = /[`~!@#_$%^&*()=|{}':;',\\\[\\\].<>/?~！@#￥……&*（）——|{}【】‘；：”“'。，、？\s]/g;
+  return str.replace(pattern, (match) => '\\' + match);
+}
+
+export const regExpSearchName = (query: any, name: any, replaceStr: string) => {
+  if (query && query.trim().length && name) {
+    //区分大小写，不区分就i匹配
+    query = escapeSpecialCharacter(query);
+    const replaceReg = new RegExp(`(${query})`, 'ig');
+    return name.replace(replaceReg, replaceStr);
+  } else {
+    return name;
+  }
+};
