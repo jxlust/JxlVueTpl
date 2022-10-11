a7b018950b5e5670dc6fb8b15154ad8133a144a1
jxlust <jxljxufe@163.com>
2022-10-11 16:04:34 +0800

    Update WaterBox.tsx

--- src/views/water-wall/components/WaterBox.tsx
+++ src/views/water-wall/components/WaterBox.tsx
@@ -1,11 +1,45 @@
-import './index.scss'
+import { InjectionKey } from 'vue';
+import './index.scss';
 
-export const ROW_GAP = 10
-export const AUTO_ROWA = 1
+export const ROW_GAP = 10;
+export const AUTO_ROWA = 1;
+
+export type WaterBoxProvide = {
+  props: any,
+  updateStatus: (v: any) => void
+}
+export const WATER_BOX_KEY: InjectionKey<WaterBoxProvide> =
+  Symbol('water-box');
 
 export default defineComponent({
-  name: 'WaterBox',
-  setup(_, { slots }) {
-    return () => <div class={'box water-wall'}>{slots.default?.()}</div>
-  },
+  name: 'water-box',
+  emits: ['first-end'],
+  setup(_, { slots, emit }) {
+    console.log('childs:', slots.default?.())
+    let childsAllKeys = [];
+    if (slots.default && slots.default()[0].children?.length) {
+      childsAllKeys = (slots.default()[0].children as any).map(item => item.id);
+    }
+    const loadEndKeys: any = [];
+    const updateStatus = (v: any) => {
+      console.log('update status:', v)
+      loadEndKeys.push(v);
+
+      if (childsAllKeys.length === loadEndKeys.length) {
+        //更新完成
+        console.log('更新完成。。。')
+        emit('first-end')
+      }
+    }
+    provide(WATER_BOX_KEY, {
+      props: 1,
+      updateStatus
+    })
+
+    return () => (
+      <div class={'box water-wall'}>
+        {slots.default?.()}
+      </div>
+    )
+  }
 })
